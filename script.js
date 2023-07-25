20b3bd1efe24fdf8584614fbf12457f80014280d
Eclaizr <claire.mrtn13@gmail.com>
2023-07-25 14:22:08 +0200

    updated

--- script.js
+++ script.js
@@ -31,3 +31,22 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+/*=============================== SKILLS OPEN/CLOSE =======================================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
