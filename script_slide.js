6b7cfa17c60905636d8f40e2384c9a49320b52a7
Eclaizr <claire.mrtn13@gmail.com>
2023-12-10 21:58:15 +0100

    portfolio section thales ok

---
+++ script_slide.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+
+function changeSlide(n) {
+    showSlide(currentSlide += n);
+}
+
+function showSlide(n) {
+    const slides = document.querySelectorAll('.slide');
+    if (n >= slides.length) {
+        currentSlide = 0;
+    } else if (n < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = n;
+    }
+    slides.forEach(slide => slide.style.transform = `translateX(-${currentSlide * 100}%)`);
+}
